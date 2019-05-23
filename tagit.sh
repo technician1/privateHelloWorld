cbf9aa2e3c24a97eb6a1ef78a9ad9ea2ddb2a73c
kevinmadden <kevin.madden@blacklynx.tech>
2019-05-23 22:45:00 +0000

    v13 adds use read-only vault, use links for largest filest before rsync, clone nimbix password

---
+++ tagit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#docker login blregistry.internationalfusioncenter.com
+v=v13
+if [ ! -z "${1}" ]
+then
+    v=$1
+fi
+docker tag blacklynx_search:${v}.nimbix.test blregistry.internationalfusioncenter.com/blacklynx_search:${v}.nimbix.test
+docker push blregistry.internationalfusioncenter.com/blacklynx_search:${v}.nimbix.test
