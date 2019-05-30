a50035a394605050b3ebd5fe2e6228c585e32f87
kevinmadden <kevin.madden@blacklynx.tech>
2019-05-30 20:53:00 +0000

    splunk on blacklynx on nimbix getting close

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM blacklynx_search:v13.nimbix
+FROM blacklynx_search:v2.nimbix
 
 # nimbix
 RUN apt-get -y update && \
--- buildit.sh
+++ buildit.sh
@@ -1,1 +1,9 @@
-docker build  -t blacklynx_search:v13.nimbix.test .
+#!/bin/bash -x
+
+v=v2
+if [ ! -z "${1}" ]
+then
+    v="${1}"
+fi
+
+docker build  -t blacklynx_search:${v}.nimbix.test .
